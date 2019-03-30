e2a077669ca0eb2945c56b96ddf1d8f1fc0f1ceb
ali126191 <ali126191@gmail.com>
2019-03-30 09:55:28 -0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9c1777c4123d4f4fcaf9.js"
+    "url": "webpack-runtime-2d62ee21a899d91b1283.js"
   },
   {
     "url": "app-5febc909fabbb12c1ba0.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dccefb2dc145a7410f26392cf8037a36"
+    "revision": "49c2858efc8d6c0539f290bf6c740be0"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
